a71c7a9764f58292eb3236f67b169fda093058f1
Eduardo Renani <eduardo.renani@hotmail.com>
2019-10-28 20:54:59 -0300

    Auto-generated commit

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  backendUrl: 'https://h07d0gez6c.execute-api.us-east-1.amazonaws.com/production'
-};
--- serverless/server.js
+++
@@ -1,4 +0,0 @@
-// server.js
-const server = require('serverless-http')
-const app = require('./src/app')
-module.exports.run = server(app)
